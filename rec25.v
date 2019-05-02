f92db6e2aa316296fb1d75518a3943e3bcab8674
Zheng Wang <zw494@cornell.edu>
2019-05-02 16:00:24 -0400

    Create rec25.v

---
+++ rec25.v
@@ -0,0 +1,24 @@
+Theorem great_peter : forall p q : Prop,
+    p /\ q -> q /\ p.
+Proof.
+  intros p q pandq.
+  destruct pandq as [ep eq].
+  split;assumption.
+Qed.
+
+Print great_peter.
+Check great_peter.
+
+Theorem bad_andrew : forall p q : Prop,
+    p \/ q -> q \/ p.
+Proof.
+  intros p q porq.
+  destruct porq as [ep | eq].
+  - right. assumption.
+  - left. assumption.
+Qed.
+
+Print bad_andrew.
+Check bad_andrew.
+
+  
